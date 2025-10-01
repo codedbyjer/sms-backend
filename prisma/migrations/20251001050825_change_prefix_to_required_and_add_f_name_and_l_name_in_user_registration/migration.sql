51f6f2bc549f25582ee7d0ec911f9bbee10235f2
soleCrft <solecraft577@gmail.com>
2025-10-01 13:48:43 +0800

    fix migration with default values for existing users

--- prisma/migrations/20251001050825_change_prefix_to_required_and_add_f_name_and_l_name_in_user_registration/migration.sql
+++ prisma/migrations/20251001050825_change_prefix_to_required_and_add_f_name_and_l_name_in_user_registration/migration.sql
@@ -10,8 +10,8 @@
 ALTER TABLE "public"."Student" ALTER COLUMN "prefix" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN     "firstName" TEXT NOT NULL,
-ADD COLUMN     "lastName" TEXT NOT NULL;
+ALTER TABLE "public"."User" ADD COLUMN "firstName" TEXT NOT NULL DEFAULT 'Admin';
+ALTER TABLE "public"."User" ADD COLUMN "lastName" TEXT NOT NULL DEFAULT 'User';
 
 -- Set Student ID sequence to start at 1000
 ALTER SEQUENCE "Student_studentId_seq" RESTART WITH 1000;
