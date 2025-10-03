7ab17f525155bbb52b76962319ee1364c244cdc7
soleCrft <solecraft577@gmail.com>
2025-10-03 14:49:02 +0800

    fix: change express limit to 10

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 5,
+    max: 10,
     message: {
         success: false,
         statusCode: 429,
